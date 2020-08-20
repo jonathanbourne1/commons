5882b6e8ad41dd5df9de19dfce409cf9e9d273d2
JonathanDevBourne <jonathan.hernandez.oliva@gmail.com>
2020-08-20 13:08:49 -0500

    service common

---
+++ src/main/java/com/restaurant/commons/CommonsApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.commons;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class CommonsApplication {
+
+}
